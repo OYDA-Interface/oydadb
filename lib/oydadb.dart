8ca05e4faa0cfd158f64448f504d5f4187663df0
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2024-06-24 13:53:08 +0000

    update pubspec

--- lib/oydadb.dart
+++ lib/oydadb.dart
@@ -3,4 +3,5 @@ library oydadb;
 export 'src/condition.dart';
 export 'src/data_manager.dart';
 export 'src/table_manager.dart';
+export 'src/oyda_interface.dart';
 export 'src/connection_manager.dart';
